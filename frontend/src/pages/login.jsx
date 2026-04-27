89269cf98242d4dfff3731c608c97161467a10f9
Solaiman <solaimanzinger@gmail.com>
2026-04-27 16:10:57 +0200

    Create welcome on the login page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,12 +1,12 @@
 import Form from "../components/form"
 
 function Login() {
-  return 
+  return ( 
     <div>
       <h1>Welcome to Matilda</h1>
       <h2>A Marketplace</h2>
       <Form route="/api/token/" method="login" />
-    </div>
+    </div> )
 }
 
 export default Login
