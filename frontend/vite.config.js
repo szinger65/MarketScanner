05a85a9ffbd084463f401c8b6d882634c7a802d7
Solaiman <solaimanzinger@gmail.com>
2026-04-27 16:48:00 +0200

    change vite version

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // This stops LightningCSS from crashing on Tailwind rules
-    cssMinify: 'esbuild', 
-  },
-  css: {
-    transformer: 'postcss',
-  },
 })
