ab23b154ab911666ffdc1ccd551b142ab836eb8f
Solaiman <solaimanzinger@icloud.com>
2026-04-27 14:11:49 +0200

    Deployement ready

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import react from "react"
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import Login from "./pages/login"
+import Register from "./pages/register"
+import NotFound from "./pages/notfound"
+import Home from "./pages/home"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function LogOut() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register />
+}
+function App() {
+  return (
+    <BrowserRouter>
+         <Routes>
+          <Route 
+            path="/"
+            element={
+             <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<LogOut />} />
+          <Route path="/register" element={<RegisterAndLogout />} />
+          <Route path="*" element={<NotFound/>} />
+         </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
