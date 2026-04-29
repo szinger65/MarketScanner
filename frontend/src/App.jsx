e08a695f004d0ba927c3613643a9f3156cfa445f
Solaiman <solaimanzinger@gmail.com>
2026-04-29 13:01:29 +0200

    create new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function LogOut() {
 function RegisterAndLogout(){
   localStorage.clear()
   return <Register />
-} //nefff ff f ff fdd d d d d dd d f
+} //nef
 function App() {
   return (
     <BrowserRouter>
